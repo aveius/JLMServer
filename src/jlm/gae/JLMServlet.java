7a54c9cf2f7c13dd8aa3eb9dbed7db9d45fb5e3b
Julien Guépin <chmod0@chmod0-desktop.(none)>
2012-02-14 02:08:38 +0100

    Write the post request parameters into server logs

---
+++ src/jlm/gae/JLMServlet.java
@@ -0,0 +1,29 @@
+package jlm.gae;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class JLMServlet extends HttpServlet {
+
+    private static final Logger log = Logger.getLogger(JLMServlet.class.getName());
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world!");
+	}
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String userName = req.getParameter("username");
+        String exoName = req.getParameter("exoname");
+        String exoLang = req.getParameter("exolang");
+        String exoSuccess = req.getParameter("success");
+
+        log.info(userName + " finished " + exoName + " in " + exoLang + " with " + exoSuccess + " success.");
+    }
+}
