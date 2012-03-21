65b107aa93134196bed4756014448e25e73a788e
Julien Guepin <julien@guepin.fr>
2012-03-21 17:14:17 +0100

    * added a json-java library * beginning of reading/printing requests in json

--- src/jlm/gae/CourseServlet.java
+++ src/jlm/gae/CourseServlet.java
@@ -1,29 +1,22 @@
 package jlm.gae;
 
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.List;
+import com.google.appengine.api.datastore.*;
+import jlm.gae.json.JSONArray;
+import jlm.gae.models.Course;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-
-import jlm.gae.models.Course;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class CourseServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		List<String> courses = new ArrayList<String>();
+        JSONArray jsonArray = new JSONArray();
 
 		DatastoreService datastore = DatastoreServiceFactory
 				.getDatastoreService();
@@ -34,11 +27,11 @@ public class CourseServlet extends HttpServlet {
 
 		for (Entity en : pq.asIterable()) {
 			Course co = new Course(en);
-			courses.add(co.getId());
+            jsonArray.put(co.getId());
 		}
 
-		ObjectOutputStream oos = new ObjectOutputStream(resp.getOutputStream());
-		oos.writeObject(courses);
-		oos.close();
+        PrintWriter pw = resp.getWriter();
+        pw.print(jsonArray.toString());
+        pw.close();
 	}
 }
--- src/jlm/gae/StudentServlet.java
+++ src/jlm/gae/StudentServlet.java
@@ -1,33 +1,54 @@
 package jlm.gae;
 
-import java.io.IOException;
-import java.io.PrintStream;
+import jlm.gae.json.JSONException;
+import jlm.gae.json.JSONObject;
+import jlm.gae.models.Exercise;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
-
-import jlm.gae.models.Exercise;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
 
 @SuppressWarnings("serial")
 public class StudentServlet extends HttpServlet {
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		String userName = req.getParameter("username");
-		String exoName = req.getParameter("exoname");
-		String exoLang = req.getParameter("exolang");
-		String courseName = req.getParameter("course");
-		int passedTests = Integer.valueOf(req.getParameter("passedtests"));
-		int totalTests = Integer.valueOf(req.getParameter("totaltests"));
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
 
-		Exercise ex = new Exercise(userName, exoName, exoLang, courseName,
-				passedTests, totalTests);
+        String jsonRequest = "";
+        BufferedReader br = req.getReader();
+        String line;
+        boolean success = false;
 
-		boolean b = ex.save();
+        while ((line = br.readLine()) != null) {
+            jsonRequest += line;
+        }
 
-		PrintStream ps = new PrintStream(resp.getOutputStream());
-		ps.print(b);
-		ps.close();
-	}
+        try {
+            JSONObject jsonObject = new JSONObject(jsonRequest);
+            String userName = jsonObject.getString("username");
+            String exoName = jsonObject.getString("exoname");
+            String exoLang = jsonObject.getString("exolang");
+            String courseName = jsonObject.getString("course");
+
+            int passedTests = Integer.valueOf(jsonObject.getString("passedtests"));
+            int totalTests = Integer.valueOf(jsonObject.getString("totaltests"));
+
+            Exercise ex = new Exercise(userName, exoName, exoLang, courseName,
+                    passedTests, totalTests);
+
+            success = ex.save();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        PrintStream ps = new PrintStream(resp.getOutputStream());
+        ps.print(success);
+        ps.close();
+    }
 }
